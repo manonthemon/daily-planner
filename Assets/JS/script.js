285fff791196dbb0d3716e84f346599332a37095
Manonthemon <klimowicz.maciej@gmail.com>
2023-01-20 14:44:47 +0000

    Added a footer. Added comments to the code

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,2 +1,11 @@
-var currentDate = moment().format(' dddd, MMMM Do');
+// Script displaying current date in the Jumbotron
+
+var currentDate = moment().format(' dddd, MMMM Do YYYY');
 document.getElementById("currentDay").innerHTML = currentDate;
+
+
+
+// TODO Add event listeners to the buttons saving entries to local storage
+// TODO Add if statement changing colors of tabs depending on current time
+
+// TODO Consider adding button to clear previous entry.
