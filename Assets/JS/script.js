967922c926efb58083e3c2c806e1884b3b581ab0
Manonthemon <klimowicz.maciej@gmail.com>
2023-01-21 11:04:38 +0000

    Added functions for filtering text areas accorgind to id value and applying different CSS classes to them to change their colors depending on time

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -6,43 +6,39 @@ var currentDate = moment().format(' dddd, MMMM Do YYYY');
 document.getElementById("currentDay").innerHTML = currentDate;
 
 // Var containing the current hour in 24 h format
-var currentTime24= new Date().getHours();
+var currentTime= new Date().getHours();
 
 // Code converting current time to 12h format
 
-var whatTime;
-if (currentTime24 >= 12) {
-    whatTime = "PM";
-} else {
-    whatTime = "AM";
-}
+// var whatTime;
+// if (currentTime24 >= 12) {
+//     whatTime = "PM";
+// } else {
+//     whatTime = "AM";
+// }
 
-var currentTime12 = "9AM"
-
-// currentTime24 % 12 + whatTime
+// var currentTime12 = currentTime24 % 12 + whatTime
 
 
 // TODO Add if statement changing colors of tabs depending on current time
 
-var timeBlock = $(".time-block") //Reference time blocks in html
-
-
-
 // There add different colors to timeBlocks depending on the time
+//Firs it selects all "textarea" elements
+//Then it filters out all those with ID whose value is > < or == to current time 
+//And it applies different classes to them accordingly.
+$("textarea").filter(function() {
+    return parseInt(this.id) < currentTime;
+}).addClass("past");
 
-//!Problem with comparing AM and PM times. 
 
-timeBlock.filter(function() {
-    return parseInt($(this).find(".hour").text()) === parseInt(currentTime12);
-}).find("textarea").addClass("present");
+$("textarea").filter(function() {
+    return parseInt(this.id) == currentTime;
+}).addClass("present");
 
-timeBlock.filter(function() {
-    return parseInt($(this).find(".hour").text()) < parseInt(currentTime12);
-}).find("textarea").addClass("past");
 
-timeBlock.filter(function() {
-    return parseInt($(this).find(".hour").text()) > parseInt(currentTime12);
-}).find("textarea").addClass("future");
+$("textarea").filter(function() {
+    return parseInt(this.id) > currentTime;
+}).addClass("future");
 
 
 
@@ -52,6 +48,9 @@ $(".saveBtn").on("click", function () {})
 
 
 // TODO Consider adding button to clear previous entry.
+
+
+
 
 
 
